4b0c70446c73cd6133c618f0e6eea080524b59f0
Julien-Alexandre Bertin-Klein <julien-alexandre.bertin-klein@tum.de>
2024-05-22 20:12:01 +0200

    fix some bugs and refactor the test case

--- submissions/ex02/Prototype/src/test/java/controller/ControllerTest.java
+++ submissions/ex02/Prototype/src/test/java/controller/ControllerTest.java
@@ -17,7 +17,7 @@ class ControllerTest {
     int customerListSize;
     int physicalBookList;
     Controller controller = new Controller();
-    Book book = new Book("Candide", "Voltaire","isbn", LocalDate.of(1759, 1, 1), "VOL01");
+    Book book = new Book("Candide", "Voltaire","isbn02", LocalDate.of(1759, 1, 1), "VOL01");
     PhysicalBook physicalBook = new PhysicalBook(book);
     Customer customer = new Customer("Vrushabh", "Jain", LocalDate.of(2004, 10, 30));
 
