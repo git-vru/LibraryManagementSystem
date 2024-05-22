abcd9c6e644289947fccb96d38cd8c9243098b1a
Julien-Alexandre Bertin-Klein <julien-alexandre.bertin-klein@tum.de>
2024-05-22 19:55:27 +0200

    Refactor Borrowing

--- submissions/ex02/Prototype/src/main/java/controller/Controller.java
+++ submissions/ex02/Prototype/src/main/java/controller/Controller.java
@@ -10,16 +10,21 @@ public class Controller {
     private View menu;
     private Scanner sc;
     private final List<Customer> customers = new ArrayList<>();
-    private final List<Book> books = new ArrayList<>();
     private final List<Borrowing> borrowing = new ArrayList<>();
+    private final Map<Book, List<PhysicalBook>> books = new HashMap<>();
 
     public Controller() {
         sc = new Scanner(System.in);
         this.menu = new MainMenu(this);
 
         this.customers.add(new Customer("Max", "Musterman", LocalDate.of(2024, 11, 1)));
-        this.books.add(new Book("Les Fleurs du Mal", "Charles Baudelaire", "isbn", LocalDate.of(1857, 6, 21), "BAU01", 5));
-        //this.menu.show();
+
+        Book book = new Book("Les Fleurs du Mal", "Charles Baudelaire", "isbn", LocalDate.of(1857, 6, 21), "BAU01");
+        this.books.put(book, new ArrayList<>());
+
+        for (int i = 0; i < 3; i++) {
+            this.books.get(book).add(new PhysicalBook(book));
+        }
     }
 
     public <T> T search(List<T> list, String query)  {
@@ -31,23 +36,23 @@ public class Controller {
     }
 
     public Book searchBook(String isbn) {
-        Optional<Book> optionalBook = this.books.stream().filter(book -> book.getIsbn().equals(isbn)).findFirst();
-        return optionalBook.map(book -> this.books.get(this.books.indexOf(book))).orElse(null);
+        Optional<Book> optionalBook = this.books.keySet().stream().filter(book -> book.getIsbn().equals(isbn)).findFirst();
+        return optionalBook.orElse(null);
     }
 
     public void createBorrowing(String bookId, String customerId) {
-        Customer customer = getCustomers().get(0);
-        PhysicalBook book = getBooks().get(0).getBookList().get(0);
+        /*Customer customer = getCustomers().get(0);
+        //PhysicalBook book = getBooks().getO;
         Borrowing borrowing = new Borrowing(book, customer, LocalDate.now(), (LocalDate.now()).plusDays(20));
 
         getBorrowing().add(borrowing);
         customer.getBorrowedList().add(borrowing);
-        book.setBorrower(customer);
+        book.setBorrower(customer);*/
     }
 
-    public boolean deleteBook(String isbn) {
-        Optional<Book> optionalBook = this.books.stream().filter(book -> book.getIsbn().equals(isbn)).findFirst();
-        return optionalBook.map(this.books::remove).orElse(false);
+    public void deleteBook(String isbn) {
+        Optional<Book> optionalBook = this.books.keySet().stream().filter(book -> book.getIsbn().equals(isbn)).findFirst();
+        this.books.remove(optionalBook.get());
     }
 
     public Customer searchCustomer(String id) {
@@ -72,7 +77,16 @@ public class Controller {
        return this.customers.remove(optionalCustomer.get());
     }
 
-    public boolean deleteBorrowing(String id) {
+    public void deletePhysicalBook (String id){
+        PhysicalBook book = null;
+        // Optional<PhysicalBook> optionalPhysicalBook = this.books.values().tostream().filter(physicalBook -> physicalBook.getId().equals(id)).findFirst();
+        for (List<PhysicalBook> list : books.values()) {
+            list.removeIf(physicalBook -> physicalBook.getId().equals(id) && physicalBook.getBorrower() != null);
+        }
+    }
+
+    public boolean addBook(String title, String author, LocalDate dateOfFirstPublication, String classificationNumber){
+
         return true;
     }
 
@@ -101,11 +115,15 @@ public class Controller {
         this.sc = sc;
     }
 
-    public List<Book> getBooks() {
+    public Map<Book, List<PhysicalBook>> getBooks() {
         return books;
     }
 
     public List<Borrowing> getBorrowing() {
         return borrowing;
+    }
+
+    public List<PhysicalBook> getPhysicalBooks(Book book) {
+        return books.get(book);
     }
 }
--- submissions/ex02/Prototype/src/test/java/controller/ControllerTest.java
+++ submissions/ex02/Prototype/src/test/java/controller/ControllerTest.java
@@ -3,10 +3,12 @@ package controller;
 import model.Book;
 import model.Borrowing;
 import model.Customer;
+import model.PhysicalBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,16 +17,17 @@ class ControllerTest {
     int bookListSize;
     int customerListSize;
     int borrowingListSize;
+    int physicalBookList;
     Controller controller = new Controller();
-    Book book = new Book("Candide", "Voltaire", "isbn02", LocalDate.of(1759, 1, 1), "VOL01", 5);
+    Book book = new Book("Candide", "Voltaire","isbn", LocalDate.of(1759, 1, 1), "VOL01");
     Customer customer = new Customer("Vrushabh", "Jain", LocalDate.of(2004, 10, 30));
-    Borrowing borrowing = new Borrowing(book.getBookList().get(0), customer);
+    PhysicalBook physicalBook = new PhysicalBook(book);
 
     @BeforeEach
     void setUp() {
-        controller.getBooks().add(book);
+        //controller.getPhysicalBooks(book).add(physicalBook);
+        controller.getBooks().put(book, new ArrayList<>());
         controller.getCustomers().add(customer);
-        controller.getBorrowing().add(borrowing);
         customerListSize = controller.getCustomers().size();
         bookListSize = controller.getBooks().size();
         borrowingListSize = controller.getBorrowing().size();
@@ -32,16 +35,18 @@ class ControllerTest {
 
     @Test
     void deleteBookSuccessfully() {
-        assertTrue(controller.deleteBook("isbn02"));
+        controller.deleteBook("isbn02");
         assertEquals(bookListSize - 1, controller.getBooks().size());
-        assertFalse(controller.getBooks().contains(book));
+        assertFalse(controller.getBooks().containsKey(book));
     }
 
     @Test
     void deleteBookUnsuccessful() {
-        assertFalse(controller.deleteBook("wrong isbn"));
+        assertThrows(NoSuchElementException.class, () -> {
+            controller.deleteBook("wrong isbn");
+        });
         assertEquals(bookListSize, controller.getBooks().size());
-        assertTrue(controller.getBooks().contains(book));
+        assertTrue(controller.getBooks().containsKey(book));
     }
 
     @Test
@@ -61,18 +66,69 @@ class ControllerTest {
     }
 
     @Test
-    void deleteBorrowingSuccessfully() {
-        assertTrue(controller.deleteBorrowing("Generated consecutively"));
-        assertEquals(borrowingListSize - 1, controller.getBorrowing().size());
-        assertFalse(controller.getBorrowing().contains(borrowing));
+    void deletePhysicalBookSuccessfully() {
+        controller.deletePhysicalBook("Generated consecutively");
+        assertEquals(physicalBookList - 1, controller.getPhysicalBooks().size());
+        assertFalse(controller.getPhysicalBooks().contains(physicalBook));
     }
 
-    /*@Test
-    void deleteBorrowingUnsuccessful() {
-        assertFalse(controller.deleteCustomer("0378184"));
-        assertEquals(customerListSize, controller.getCustomers().size());
-        assertTrue(controller.getCustomers().contains(customer));
-    }*/
+    @Test
+    void deletePhysicalBookUnsuccessful() {
+        assertThrows(NoSuchElementException.class, () -> {
+            controller.deletePhysicalBook("wrong id");
+        });
+        assertEquals(physicalBookList, controller.getPhysicalBooks().size());
+        assertTrue(controller.getPhysicalBooks().contains(physicalBook));
+    }
 
+    @Test
+    void addBookSuccessfully() {
+        assertTrue(controller.addBook(book.getTitle(), book.getAuthor(), book.getPublicationDate(), book.getClassificationNumber()));
+        assertEquals(bookListSize + 1, controller.getBooks().size());
+        assertFalse(controller.getBooks().containsKey(book));
+    }
+
+    @Test
+    void addBookUnsuccessful() {
+        assertThrows(NoSuchElementException.class, () -> {
+            controller.deletePhysicalBook("wrong id");
+        });
+        assertEquals(physicalBookList, controller.getPhysicalBooks().size());
+        assertTrue(controller.getPhysicalBooks().contains(physicalBook));
+    }
+
+    @Test
+    void modifyBookSuccessfully() {}
+
+    @Test
+    void modifyBookUnsuccessful() {}
+
+    @Test
+    void addCustomerSuccessfully() {
+        assertTrue(controller.addBook(book.getTitle(), book.getAuthor(), book.getPublicationDate(), book.getClassificationNumber()));
+        assertEquals(bookListSize + 1, controller.getBooks().size());
+        assertFalse(controller.getBooks().containsKey(book));
+    }
+
+    @Test
+    void addCustomerUnsuccessful() {}
+
+    @Test
+    void modifyCustomerSuccessfully() {}
+
+    @Test
+    void modifyCustomerUnsuccessful() {}
+
+    @Test
+    void addPhysicalBookSuccessfully() {}
+
+    @Test
+    void addPhysicalBookUnsuccessful() {}
+
+    @Test
+    void modifyPhysicalBookSuccessfully() {}
+
+    @Test
+    void modifyPhysicalBookUnsuccessful() {}
 
 }
