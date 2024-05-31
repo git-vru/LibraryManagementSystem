12f702691b0e0f49a84e7c2ee4d9fe153cdf8b88
git-vru <vrushabh.jain@tum.de>
2024-05-31 16:54:37 +0200

    made the csv file reader class as well as implemented the function

---
+++ src/main/java/CSVreader.java
@@ -0,0 +1,88 @@
+import controller.Controller;
+import model.Book;
+import model.BookCopy;
+import model.Customer;
+
+import java.awt.*;
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CSVreader {
+
+    private List<Book> importedBooks;
+    private List<BookCopy> importedBookCopies;
+    private List<Customer> importedCustomers;
+    private Controller controller;
+
+    public CSVreader() {
+        this.importedBooks = new ArrayList<>();
+        this.importedBookCopies = new ArrayList<>();
+        this.importedCustomers = new ArrayList<>();
+        this.controller = new Controller();
+    }
+    public List<Book> makeBooks(String path) throws FileNotFoundException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            reader.readLine();
+
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 5) {
+                    String classificationNumber = parts[0].trim();
+                    String isbn = parts[1].trim();
+                    String title = parts[2].trim();
+                    String author = parts[3].trim();
+                    LocalDate publicationDate = LocalDate.parse(parts[4].trim());
+
+                    Book book = new Book(title, author, isbn, publicationDate, classificationNumber);
+                    importedBooks.add(book);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return importedBooks    ;
+    }
+    public List<BookCopy> makeBookCopies(String path) throws FileNotFoundException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 1) {
+                    String isbn = parts[0].trim();
+                    BookCopy bookCopy = new BookCopy(controller.searchBook(isbn));
+                    importedBookCopies.add(bookCopy);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return importedBookCopies;
+    }
+    public List<Customer> makeCustomer(String path) throws FileNotFoundException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            reader.readLine();
+
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 3) {
+                    String fName = parts[0].trim();
+                    String lName = parts[1].trim();
+                    LocalDate DOB = LocalDate.parse(parts[2].trim());
+
+                    Customer customer = new Customer(fName,lName,DOB);
+                    importedCustomers.add(customer);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return importedCustomers;
+    }
+
+}
