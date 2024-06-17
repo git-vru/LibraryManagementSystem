89a6f63f42d12431cb96bb36769306d1904513c2
Samuel Kemmler <go34taw@mytum.de>
2024-06-17 19:45:25 +0200

    deleting variable, solved issue

--- src/main/java/view/MainMenu.java
+++ src/main/java/view/MainMenu.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainMenu extends View {
-    private char inputChar;
 
     private final List<String> options;
 
@@ -26,8 +25,8 @@ public class MainMenu extends View {
     }
 
     @Override
-    public void show() {inputChar = super.promptMenu(name, this.options);
-        switch (inputChar) {
+    public void show() {
+        switch (super.promptMenu(name, this.options)) {
             case '0' -> controller.setMenu(new BookMenu(controller, this));
             case '1' -> controller.setMenu(new CustomerMenu(controller, this));
             case '2' -> controller.setMenu(new BorrowingMenu(controller, this));
